d1781789c08fd894a5031a52e85b9cc65871a916
Piotr Kapcia <Piotr_Kapcia@epam.com>
2019-03-21 17:02:13 +0100

    Results

--- src/main/java/com/epam/H2oPredictor.java
+++ src/main/java/com/epam/H2oPredictor.java
@@ -2,6 +2,7 @@ package com.epam;
 
 import hex.genmodel.*;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class H2oPredictor {
 
@@ -10,5 +11,11 @@ public class H2oPredictor {
         String path = classpath.split(";")[0];
         MojoReaderBackend mojoReaderBackend = MojoReaderBackendFactory.createReaderBackend(path);
         MojoModel mojoModel = ModelMojoReader.readFrom(mojoReaderBackend);
+        double[] row = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        double[] preds = new double[10];
+        double[] results = mojoModel.score0(row, preds);
+        System.out.println(Arrays.toString(row));
+        System.out.println(Arrays.toString(preds));
+        System.out.println(Arrays.toString(results));
     }
 }

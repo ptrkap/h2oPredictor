e143ea6fac27de962589ae474ab1522bcac7cb57
Piotr Kapcia <Piotr_Kapcia@epam.com>
2019-03-21 16:40:09 +0100

    Up and running

--- src/main/java/com/epam/H2oPredictor.java
+++ src/main/java/com/epam/H2oPredictor.java
@@ -1,8 +1,14 @@
 package com.epam;
 
+import hex.genmodel.*;
+import java.io.IOException;
+
 public class H2oPredictor {
 
-    public static void main(String[] args) {
-        System.out.println("foobar");
+    private static final String PATH = System.getProperty("user.dir") + "\\src\\main\\resources";
+
+    public static void main(String[] args) throws IOException {
+        MojoReaderBackend mojoReaderBackend = MojoReaderBackendFactory.createReaderBackend(PATH);
+        MojoModel mojoModel = ModelMojoReader.readFrom(mojoReaderBackend);
     }
 }

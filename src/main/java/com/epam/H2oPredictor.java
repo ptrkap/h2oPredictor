a2320f72eb30e0811231c0c5802a5e5b2decffb3
Piotr Kapcia <Piotr_Kapcia@epam.com>
2019-03-21 16:47:03 +0100

    Classpath

--- src/main/java/com/epam/H2oPredictor.java
+++ src/main/java/com/epam/H2oPredictor.java
@@ -5,10 +5,10 @@ import java.io.IOException;
 
 public class H2oPredictor {
 
-    private static final String PATH = System.getProperty("user.dir") + "\\src\\main\\resources";
-
     public static void main(String[] args) throws IOException {
-        MojoReaderBackend mojoReaderBackend = MojoReaderBackendFactory.createReaderBackend(PATH);
+        String classpath = System.getProperty("java.class.path");
+        String path = classpath.split(";")[0];
+        MojoReaderBackend mojoReaderBackend = MojoReaderBackendFactory.createReaderBackend(path);
         MojoModel mojoModel = ModelMojoReader.readFrom(mojoReaderBackend);
     }
 }
